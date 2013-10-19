963ac70c1ec2e0c8762f0c56c34571d5e8a2a60e
Matt Witkowski <mwitkows@stevens.edu>
2013-10-19 16:41:56 -0400

    Did temporary PlayerController. It should extend a class called Controller.

---
+++ source/PlayerController.h
@@ -0,0 +1,54 @@
+#include "state.h"
+#include "hashmap.h"
+#include "actor.h"
+#include <string>
+#include "statemachine.h"
+
+using namespace std;
+
+class PlayerController
+{
+    private:
+        Hashmap<string, State>* _states;
+        Actor* _actor;
+        StateMachine* _stateMachine;
+    public:
+        PlayerController(Actor actor);
+        void input(DynamicArray<string> command);
+        void intialize();
+};
+
+PlayerController::PlayerController()
+{
+}
+
+void PlayerController::initialize(Actor* actor)
+{
+    _states = new Hashmap<string, State>();
+    _stateMachine = new StateMachine();
+    _actor = actor;
+
+    DrinkState drinkState;
+    MineState mineState;
+    SingState singState;
+    BrawlState brawlState;
+    SleepState sleepState;
+    IdleState idleState;
+
+    _states.add("drink", drinkState);
+    _states.add("mine", mineState);
+    _states.add("sing", singState);
+    _states.add("brawl", brawlState);
+    _states.add("sleep", sleepState);
+    _states.add("idle", idleState);
+}
+
+
+void PlayerController::input(DynamicArray<string> multiInput)
+{
+    for(uint i = 0; i < multiInput.getSize(); i++)
+    {
+        stateMachine->switchState(stateMachine->getCurrentState(), 
+                                _states->get(multiInput.get(i)));
+    }
+}
