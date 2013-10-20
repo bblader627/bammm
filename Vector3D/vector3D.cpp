b9c886696d563d3fbebad62be52e7756d93d49e6
bradley_crusco <bcrusco627@gmail.com>
2013-10-20 12:44:27 -0400

    Moved comments from .h to .cpp. Made comments doc style. Changed variable names to follow C++ and style guide conventions. Organized code in the header to match the organization of the cpp.

--- Vector3D/vector3D.cpp
+++ Vector3D/vector3D.cpp
@@ -1,4 +1,4 @@
-/*
+/**
  * CS585
  *
  * Team MMA
@@ -42,6 +42,11 @@ namespace bammm
 		_z = z;
 	}
 
+	/**
+	 set
+	 @Pre-Condition- Takes in new values for x,y,z components
+	 @Post-Condition- Returns nothing but sets x,y,z components to new values
+	 */
 	void Vector3D::set(float x, float y, float z)
 	{
 		_x = x;
@@ -49,6 +54,13 @@ namespace bammm
 		_z = z;
 	}
 
+	/**
+	 normalize
+	 @Pre-Condition-Takes no arguments
+	 @Post-Condition- returns nothing, just normalizea this Vector
+	 Normal
+	 A = {x,y,z} / |a| |a| = magnitude
+	 */
 	void Vector3D::normalize()
 	{
 		float normal = magnitude();
@@ -57,6 +69,12 @@ namespace bammm
 		_z = _z / normal;
 	}
 
+	/**
+	 magnitude
+	 @Pre-Condition-Takes no arguments
+	 @Post-Condition- returns vectors length
+	 |a| = sqrt( x^2 + y^2 + z^2)
+	 */
 	float Vector3D::magnitude()
 	{
 		return sqrt((_x * _x) + (_y * _y) + (_z * _z));
@@ -67,19 +85,39 @@ namespace bammm
 		return (_x * _x) + (_y * _y) + (_z * _z);
 	}
 
+	/**
+	 dotProduct
+	 @Pre-Condition-Takes a Vector and will perform a Dot Product
+	 @Post-Condition- It will return the resulting int
+	 Dot Product
+	 A*B = Ax*Bx + Ay*By + Az*Bz
+	 */
 	float Vector3D::dotProduct(Vector3D &vect)
 	{
 		return (_x * vect.x()) + (_y * vect.y()) + (_z * vect.z());
 	}
 
+	/**
+	 divideVector
+	 @Pre-Condition- Takes in scalar value
+	 @Post-Condition- returns a Vector with representing the division
+	 Vector Division
+	 A/n = {Ax/n,Ay/n,Az/n}  n = scalar value
+	 */
 	void Vector3D::divideVector(int n)
 	{
 		_x = _x / n;
 		_y = _y / n;
 		_z = _z / n;
-
 	}
 
+	/**
+	 multiplyVector
+	 @Pre-Condition- Takes in scalar value
+	 @Post-Condition- returns a Vector with representing the multiplication
+	 Vector Multiplication
+	 A*n = {Ax*n,Ay*n,Az*n}  n = scalar value
+	 */
 	void Vector3D::multiplyVector(int n)
 	{
 		_x = _x * n;
@@ -87,13 +125,26 @@ namespace bammm
 		_z = _z * n;
 	}
 
-	Vector3D* Vector3D::crossProduct(Vector3D &vect)
+	/**
+	 crossProduct
+	 @Pre-Condition- Takes a Vector and performs a Cross Product
+	 @Post-Condition- returns a vector representing the result
+	 Cross Product
+	 AxB = [(Ay * Bz) - (Az - By), (Ax * Bz) - (Az - Bx), (Ax * By) - (Ay -Bx)]
+	 */
+	Vector3D* Vector3D::crossProduct(Vector3D &vector)
 	{
-		return Vector3D((_y * vect.z()) - (_z - vect.y()),
-				(_x * vect.z()) - (_z - vect.x()),
-				(_x * vect.y()) - (_y - vect.x()));
+		return Vector3D((_y * vector.z()) - (_z - vector.y()),
+				(_x * vector.z()) - (_z - vector.x()),
+				(_x * vector.y()) - (_y - vector.x()));
 
 	}
+
+	/**
+	 toString
+	 @Pre-Condition- Takes no parameters
+	 @Post-Condition- returns a String representing a vector
+	 */
 	string Vector3D::toString()
 	{
 		string output;
@@ -107,10 +158,14 @@ namespace bammm
 		return output;
 	}
 
-//Operator Overloads
-	bool Vector3D::operator==(const Vector3D &vect)
+	/**
+	 Overload ==
+	 @Pre-Condition-Takes a vector
+	 @Post-Condition- compares the equality of the vectors
+	 */
+	bool Vector3D::operator==(const Vector3D &vector)
 	{
-		if ((_x == vect.x()) && (_y == vect.y()) && (_z == vect.z()))
+		if ((_x == vector.x()) && (_y == vector.y()) && (_z == vector.z()))
 		{
 			return true;
 		}
@@ -120,9 +175,14 @@ namespace bammm
 		}
 	}
 
-	Vector3D Vector3D::operator!=(const Vector3D &vect)
+	/**
+	 Overload !=
+	 @Pre-Condition-Takes a vector
+	 @Post-Condition- compares the equality of the vectors
+	 */
+	Vector3D Vector3D::operator!=(const Vector3D &vector)
 	{
-		if ((_x == vect.x()) && (_y == vect.y()) && (_z == vect.z()))
+		if ((_x == vector.x()) && (_y == vector.y()) && (_z == vector.z()))
 		{
 			return false;
 		}
@@ -132,27 +192,47 @@ namespace bammm
 		}
 	}
 
-	Vector3D Vector3D::operator+(const Vector3D &vect)
+	/**
+	 Overload +
+	 @Pre-Condition-Takes a vector
+	 @Post-Condition- adds two vectors
+	 */
+	Vector3D Vector3D::operator+(const Vector3D &vector)
 	{
-		return Vector3D(_x + vect.x(), _y + vect.y(), _z + vect.z());
+		return Vector3D(_x + vector.x(), _y + vector.y(), _z + vector.z());
 	}
 
-	Vector3D Vector3D::operator+=(const Vector3D &vect)
+	/**
+	 Overload +=
+	 @Pre-Condition-Takes a vector
+	 @Post-Condition- adds two vectors together stores it in original
+	 */
+	Vector3D Vector3D::operator+=(const Vector3D &vector)
 	{
-		_x += vect.x();
-		_y += vect.y();
-		_z += vect.z();
+		_x += vector.x();
+		_y += vector.y();
+		_z += vector.z();
 	}
 
-	Vector3D Vector3D::operator-(const Vector3D &vect)
+	/**
+	 Overload -
+	 @Pre-Condition-Takes a vector
+	 @Post-Condition- subtracts two vectors
+	 */
+	Vector3D Vector3D::operator-(const Vector3D &vector)
 	{
-		return Vector3D(_x - vect.x(), _y - vect.y(), _z - vect.z());
+		return Vector3D(_x - vector.x(), _y - vector.y(), _z - vector.z());
 	}
 
-	Vector3D Vector3D::operator-=(const Vector3D &vect)
+	/**
+	 Overload -=
+	 @Pre-Condition-Takes a vector
+	 @Post-Condition- subtracts two vectors together stores it in original
+	 */
+	Vector3D Vector3D::operator-=(const Vector3D &vector)
 	{
-		_x -= vect.x();
-		_y -= vect.y();
-		_z -= vect.z();
+		_x -= vector.x();
+		_y -= vector.y();
+		_z -= vector.z();
 	}
 }
--- Vector3D/vector3D.h
+++ Vector3D/vector3D.h
@@ -19,172 +19,78 @@
 #include <math.h>
 #include <iostream>
 #include <string>
-using namespace std;
 
 #ifndef NULL
 #define NULL (void *)0
 #endif
 
-class Vector3D
+using namespace std;
+namespace bammm
 {
-	private:
-		float _x;
-		float _y;
-		float _z;
+	class Vector3D
+	{
+		private:
+			float _x;
+			float _y;
+			float _z;
 
-	public:
-		//Constant Variables
-		const Vector3D UP;
-		const Vector3D DOWN;
-		const Vector3D LEFT;
-		const Vector3D RIGHT;
-		const Vector3D ZERO;
+		public:
+			//Constant Variables
+			const Vector3D UP;
+			const Vector3D DOWN;
+			const Vector3D LEFT;
+			const Vector3D RIGHT;
+			const Vector3D ZERO;
 
-		//Constructors		
-		Vector3D();
-		Vector3D(float x, float y, float z);
+			Vector3D();
+			Vector3D(float x, float y, float z);
+			~Vector3D();
+			void set(float a, float b, float c);
+			void normalize();
+			float magnitude();
+			float dotProduct(Vector3D *vector);
+			void divideVector(int n);
+			void multiplyVector(int n);
+			Vector3D* crossProduct(Vector3D *vector);
+			string toString();
 
-		//Destructor
-		~Vector3D();
+			bool operator==(const Vector3D *vector);
+			bool operator!=(const Vector3D *vector);
+			bool operator+(const Vector3D *vector);
+			bool operator+=(const Vector3D *vector);
+			bool operator-(const Vector3D *vector);
+			bool operator-=(const Vector3D *vector);
 
-		//Operator Overloads
-		/*
-		 Overload ==
-		 @Pre-Condition-Takes a vector
-		 @Post-Condition- compares the equality of the vectors
-		 */
-		bool operator==(const Vector3D *vect);
-		/*
-		 Overload !=
-		 @Pre-Condition-Takes a vector
-		 @Post-Condition- compares the equality of the vectors
-		 */
-		bool operator!=(const Vector3D *vect);
-		/*
-		 Overload +
-		 @Pre-Condition-Takes a vector
-		 @Post-Condition- adds two vectors
-		 */
-		bool operator+(const Vector3D *vect);
-		/*
-		 Overload +=
-		 @Pre-Condition-Takes a vector
-		 @Post-Condition- adds two vectors together stores it in original
-		 */
-		bool operator+=(const Vector3D *vect);
-		/*
-		 Overload -
-		 @Pre-Condition-Takes a vector
-		 @Post-Condition- subtracts two vectors
-		 */
-		bool operator-(const Vector3D *vect);
-		/*
-		 Overload -=
-		 @Pre-Condition-Takes a vector
-		 @Post-Condition- subtracts two vectors together stores it in original
-		 */
-		bool operator-=(const Vector3D *vect);
+			inline float x() const
+			{
+				return _x;
+			}
 
-		//Functions
-		/*
-		 normalize
-		 @Pre-Condition-Takes no arguments
-		 @Post-Condition- returns nothing, just normalizea this Vector
-		 Normal
-		 A = {x,y,z} / |a| |a| = magnitude
-		 */
-		void normalize();
+			inline float y() const
+			{
+				return _y;
+			}
 
-		/*
-		 magnitude
-		 @Pre-Condition-Takes no arguments
-		 @Post-Condition- returns vectors length
-		 |a| = sqrt( x^2 + y^2 + z^2)
-		 */
-		float magnitude();
+			inline float z() const
+			{
+				return _z;
+			}
 
-		/*
-		 set
-		 @Pre-Condition- Takes in new values for x,y,z components
-		 @Post-Condition- Returns nothing but sets x,y,z components to new values
-		 */
-		void set(float a, float b, float c);
-		/*
-		 Accessors/Mutators
-		 Easier than using set and get Functions
-		 Vector require multiple acces to these variables.
-		 */
-		inline float x() const
-		{
-			return _x;
-		}
+			inline float& x()
+			{
+				return _x;
+			}
 
-		inline float y() const
-		{
-			return _y;
-		}
+			inline float& y()
+			{
+				return _y;
+			}
 
-		inline float z() const
-		{
-			return _z;
-		}
-
-		inline float& x()
-		{
-			return _x;
-		}
-
-		inline float& y()
-		{
-			return _y;
-		}
-
-		inline float& z()
-		{
-			return _z;
-		}
-		/*
-		 dotProduct
-		 @Pre-Condition-Takes a Vector and will perform a Dot Product
-		 @Post-Condition- It will return the resulting int
-		 Dot Product
-		 A*B = Ax*Bx + Ay*By + Az*Bz
-		 */
-		float dotProduct(Vector3D *vect);
-		/*
-		 divideVector
-		 @Pre-Condition- Takes in scalar value
-		 @Post-Condition- returns a Vector with representing the division
-		 Vector Division
-		 A/n = {Ax/n,Ay/n,Az/n}  n = scalar value
-		 */
-		void divideVector(int n);
-
-		/*
-		 multiplyVector
-		 @Pre-Condition- Takes in scalar value
-		 @Post-Condition- returns a Vector with representing the multiplication
-		 Vector Multiplication
-		 A*n = {Ax*n,Ay*n,Az*n}  n = scalar value
-		 */
-		void multiplyVector(int n);
-
-		/*
-		 crossProduct
-		 @Pre-Condition- Takes a Vector and performs a Cross Product
-		 @Post-Condition- returns a vector representing the result
-		 Cross Product
-		 AxB = [(Ay * Bz) - (Az - By), (Ax * Bz) - (Az - Bx), (Ax * By) - (Ay -Bx)]
-		 */
-		Vector3D* crossProduct(Vector3D *vect);
-
-		/*
-		 toString
-		 @Pre-Condition- Takes no parameters
-		 @Post-Condition- returns a String representing a vector
-		 */
-		string toString();
-};
-
+			inline float& z()
+			{
+				return _z;
+			}
+	};
+}
 #endif
 
