5cdf8b5ed2d1ff8740e6ed757280ff9887693896
Matt Witkowski <mwitkows@stevens.edu>
2013-10-20 16:37:07 -0400

    Added some logic to AiController

--- source/AiController.h
+++ source/AiController.h
@@ -14,10 +14,18 @@ namespace bammm
             Hashmap<string, State>* _states;
             Actor* _actor;
             StateMachine* _stateMachine;
+            DrinkState* drinkState;
+            MineState* mineState;
+            SingState* singState;
+            BrawlState* brawlState;
+            SleepState* sleepState;
+            IdleState* idleState;
+
         public:
             AiController(Actor actor);
             void intialize();
             void update();
+            uint counter = 0;
     };
 
     AiController::AiController()
@@ -29,26 +37,66 @@ namespace bammm
         _states = new Hashmap<string, State>();
         _stateMachine = new StateMachine();
         _actor = actor;
-    
-        DrinkState drinkState;
-        MineState mineState;
-        SingState singState;
-        BrawlState brawlState;
-        SleepState sleepState;
-        IdleState idleState;
+        
+        drinkState = new DrinkState;
+        mineState = new MineState;
+        singState = new SingState;
+        brawlState = new BrawlState;
+        sleepState = new SleepState;
+        idleState = new IdleState;
 
-        _states.add("drink", drinkState);
-        _states.add("mine", mineState);
-        _states.add("sing", singState);
-        _states.add("brawl", brawlState);
-        _states.add("sleep", sleepState);
-        _states.add("idle", idleState);
+        _states.add("drink", *drinkState);
+        _states.add("mine", *mineState);
+        _states.add("sing", *singState);
+        _states.add("brawl", *brawlState);
+        _states.add("sleep", *sleepState);
+        _states.add("idle", *idleState);
     }
 
 
     void AiController::update()
     {
+        string newState;
+        int random;
+
+        if(counter == 0)
+        {
+            newState = "idle";
+            counter = 1;
+        }
+        else if(counter == 1 || counter == 3)
+        {
+           newState = "drink"; 
+           counter++;
+        }
+        else if(counter == 2)
+        {
+            newState = "mine";
+            counter++;
+        }
+        else if(counter == 3)
+        {
+            random = rand() % 100 + 1;
+            if(random <= 33)
+            {
+                newState = "sleep";
+                counter = 0;
+            }
+            else
+            {
+                if(random <= 66)
+                {
+                    newState = "brawl";
+                }
+                else
+                {
+                    newState = "sing";
+                }
+                counter = 2;
+            }
+        }
+
         stateMachine->switchState(stateMachine->getCurrentState(), 
-                                 _states->get(multiInput.get(i)));
+                                 _states->get(newState));
     }
 }
