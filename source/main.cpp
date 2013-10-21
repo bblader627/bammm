24eb336af96c7223c2b2c747ea81974bfa71e50f
Michael Abramo <abramomichael@gmail.com>
2013-10-21 06:01:44 -0400

    uploaded main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+//#include "JSON/JSONParser.h"
+#include "IStateCallback.h"
+
+using namespace bammm;
+
+int main()
+{
+
+//print welcome
+
+//allow player to create dwarf or automatically create one
+
+//create controllers
+
+//start dwarf in IDLE
+
+//provide option for state change
+
+//tick
+
+	return 0;
+}
+
+void printWelcome()
+{
+	cout << "================================================" << endl;
+	cout << "Welcome to BAMMM -  Alpha v0.1" << endl;
+	cout
+			<< "Creators: Alvaro Home - Matthew Konstantinou - Matthew Witkowski - Bradley Crusco - Michael Abramo"
+			<< endl;
+	cout << "================================================" << endl;
+}
+
+bool createActor()
+{
+
+	return false;
+}
