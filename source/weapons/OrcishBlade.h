fd2313202f0ae30f40698c98d7e2c0ea06817c7b
Michael Abramo <abramomichael@gmail.com>
2013-11-01 23:25:59 -0400

    added OrcishBlade.h

---
+++ source/weapons/OrcishBlade.h
@@ -0,0 +1,71 @@
+#ifndef STEIN_H_
+#define STEIN_H_
+
+#include "MeleeWeapon.h"
+#include "../TestSuite/time.cpp"
+
+#ifndef UNIT
+#define UINT
+typedef unsigned int uint;
+#endif
+
+namespace bammm
+{
+	class OrcishBlade : public MeleeWeapon
+	{
+		private:
+			WeaponData* weaponData;
+			uint timer;
+			Time time;
+
+		public:
+			OrcishBlade();
+			OrcishBlade(WeaponData* wd);
+			~OrcishBlade();
+			int attack();
+			bool canAttack();
+	};
+
+	OrcishBlade::OrcishBlade()
+	{
+		int damage = 6;
+		int cooldown = 2;
+
+		weaponData = new WeaponData(damage, cooldown, "", "melee");
+		timer = 0;;
+	}
+
+	OrcishBlade::OrcishBlade(WeaponData* wd)
+	{
+		weaponData = wd;
+		timer = 0;
+	}
+
+	OrcishBlade::~OrcishBlade()
+	{
+		delete weaponData;
+	}
+
+	int OrcishBlade::attack()
+	{
+		time = time.current();
+		if (canAttack())
+		{
+			timer = time.getSeconds() + weaponData->getFireRate();
+			return weaponData->getDamage();
+		}
+		return 0;
+	}
+
+	bool OrcishBlade::canAttack()
+	{
+		time = time.current();
+		if (time.getSeconds() < timer)
+		{
+			return false;
+		}
+
+		return true;
+	}
+}
+#endif
