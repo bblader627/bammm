f796ceecc68baac491c1ac81d7332a372e11633d
Matthew Konstantinou <matt.konstantinou@gmail.com>
2013-11-02 21:36:48 -0400

    created factory and ActorFactory classes for mass production via JSONParser - nothing implemented yet

---
+++ source/factories/ActorFactory.h
@@ -0,0 +1,29 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *
+ * Description:
+ * ActorFactory header file.
+ *
+ * Last Modified: Matthew Konstantinou
+ *
+ */
+
+#include <iostream>
+#include "../resources/dynamicarray.h"
+#include "factory.h"
+#include "../actors/actor.h"
+
+using namespace bammm;
+
+namespace bammm
+{
+	class ActorFactory : public Factory
+	{
+		//THINGS GO HERE
+	};
+}
---
+++ source/factories/factory.h
@@ -0,0 +1,28 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *
+ * Description:
+ * ActorFactory header file.
+ *
+ * Last Modified: Matthew Konstantinou
+ *
+ */
+
+#include <iostream>
+#include "../JSON/JSONParser.h"
+#include "../resources/dynamicarray.h"
+
+using namespace bammm;
+
+namespace bammm
+{
+	class Factory
+	{
+		//HOW PARSE WORK
+	};
+}
