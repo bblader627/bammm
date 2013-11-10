4552d0d4f97cf16fbf07eab1da0710ae968f34ca
bradley_crusco <bcrusco627@gmail.com>
2013-11-09 20:33:28 -0500

    Framework for BlackboardRecord is now there.

--- Source/Blackboard/BlackboardRecord.h
+++ Source/Blackboard/BlackboardRecord.h
@@ -8,8 +8,33 @@
 #ifndef BLACKBOARDRECORD_H_
 #define BLACKBOARDRECORD_H_
 
+#include <string>
 
+using namespace std;
+//eh?
 
+namespace bammm
+{
+	class BlackboardRecord
+	{
+		private:
+			enum EnumRecordType
+			{
+				Invalid, Attacking
+			};
+			EnumRecordType _enumType;
+			string _subjectID;
+			string _targetID;
+			float _data;
 
+		public:
+			BlackboardRecord(EnumRecordType enumType, string subjectID);
+			BlackboardRecord(EnumRecordType enumType, string subjectID, string targetID, float _value);
+			EnumRecordType getEnumRecordType();
+			string getSubjectID();
+			string getTargetID();
+			float getData();
+	};
+}
 
 #endif /* BLACKBOARDRECORD_H_ */
