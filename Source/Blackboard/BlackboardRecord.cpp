cacb7633802c4924d10f43b604dc9def610ae339
bradley_crusco <bcrusco627@gmail.com>
2013-11-09 20:52:11 -0500

    Added accessors to BlackboardRecord

---
+++ Source/Blackboard/BlackboardRecord.cpp
@@ -0,0 +1,51 @@
+/*
+ * BlackboardRecord.cpp
+ *
+ *  Created on: Nov 9, 2013
+ *      Author: bradley
+ */
+
+#include "BlackboardRecord.h"
+
+namespace bammm
+{
+	BlackboardRecord::BlackboardRecord(EnumRecordType enumType,
+			string subjectID)
+	{
+		_enumType = enumType;
+		_subjectID = subjectID;
+		_targetID = NULL;
+		_data = 0;
+	}
+
+	BlackboardRecord::BlackboardRecord(EnumRecordType enumType,
+			string subjectID, string targetID, float data)
+	{
+		_enumType = enumType;
+		_subjectID = subjectID;
+		_targetID = targetID;
+		_data = data;
+	}
+
+	// Maybe I shouldn't return EnumRecordType? just the int?
+	BlackboardRecord::EnumRecordType BlackboardRecord::getEnumRecordType()
+	{
+
+	}
+
+	string BlackboardRecord::getSubjectID()
+	{
+		return _subjectID;
+	}
+
+	string BlackboardRecord::getTargetID()
+	{
+		return _targetID;
+	}
+
+	float BlackboardRecord::getData()
+	{
+		return _data;
+	}
+}
+
