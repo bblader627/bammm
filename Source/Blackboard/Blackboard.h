f2d0dec61ee3911fbeea663071f91a302cb23533
bradley_crusco <bcrusco627@gmail.com>
2013-11-09 20:26:17 -0500

    Framework for Blackboard in place.

--- Source/Blackboard/Blackboard.h
+++ Source/Blackboard/Blackboard.h
@@ -8,8 +8,28 @@
 #ifndef BLACKBOARD_H_
 #define BLACKBOARD_H_
 
+#include "BlackboardRecord.h"
 
+namespace bammm
+{
+	class Blackboard
+	{
+		private:
+			DynamicArray<BlackboardRecord> _ListOfRecords;
 
+		public:
+			bool addRecord(EnumRecordType type, string subjectID);
+			bool addRecord(EnumRecordType type, string subjectID, string targetID, float data);
+			bool removeRecord(EnumRecordType);
+			bool removeRecord(string subjectID);
+			bool removeAll(EnumRecordType);
+			int countRecords(EnumRecordType type);
+			int countRecords(EnumRecordType type, string target);
+			float getData(EnumRecordType type);
+			float getData(EnumRecordType type, string target);
+			//Functions to replace records?
+	};
+}
 
 
 #endif /* BLACKBOARD_H_ */
