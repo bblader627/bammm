51ae574eb77b55bd47d4a71db25d2989e3669408
bradley_crusco <bcrusco627@gmail.com>
2013-11-17 18:54:43 -0500

    Added author comments, added docstyle comments, changed variable names to match our style guide, fixed white space issues, reorganized the code slightly.

--- Source/States/SleepState.h
+++ Source/States/SleepState.h
@@ -1,3 +1,17 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *	Bradley Crusco
+ * Description:
+ * SleepState header file.
+ *
+ */
+
 #ifndef SLEEPSTATE_H_
 #define SLEEPSTATE_H_
 
@@ -15,73 +29,105 @@ using namespace std;
 
 namespace bammm
 {
-    class SleepState : public State
-    {
-        public:
-            SleepState(Actor* actor);
-            SleepState(Actor* actor, IStateCallback* statemachine);
-            void setup();
-            void breakdown();
-            void tick(float dTime);
-            void switchState(string nextState);
-			string toString();
+	class SleepState: public State
+	{
+		private:
+			static const int _hoursToSleep = 8;
+			int _timeSlept;
 
-        private:
-            static const int hoursToSleep = 8;
-            int timeSlept;
-    };
+		public:
+			SleepState(Actor* actor);
+			SleepState(Actor* actor, IStateCallback* stateMachine);
+
+			/**
+			 setup
+			 @Pre-Condition- No input
+			 @Post-Condition- Sets up the state
+			 */
+			void setup();
+
+			/**
+			 breakdown
+			 @Pre-Condition- No input
+			 @Post-Condition- Performs a breakdown on the state
+			 */
+			void breakdown();
+
+			/**
+			 tick
+			 @Pre-Condition- Takes in a float deltaTime
+			 @Post-Condition- Executes a tick of length deltaTime
+			 */
+			void tick(float deltaTime);
+
+			/**
+			 switchState
+			 @Pre-Condition- Takes in a string nextState
+			 @Post-Condition- The current state is switched to the given nextState
+			 */
+			void switchState(string nextState);
+
+			/**
+			 toString
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns a string representation of the state
+			 */
+			string toString();
+	};
 
 	SleepState::SleepState(Actor* actor)
 	{
 		_actor = actor;
 	}
 
-	SleepState::SleepState(Actor* actor, IStateCallback* statemachine)
+	SleepState::SleepState(Actor* actor, IStateCallback* stateMachine)
 	{
 		_actor = actor;
-		registerTransitionCallback(statemachine);
+		registerTransitionCallback(stateMachine);
 	}
 
-    void SleepState::setup()
-    {
-        timeSlept = 0;
-    }
+	void SleepState::setup()
+	{
+		_timeSlept = 0;
+	}
 
-    void SleepState::breakdown()
-    {
-    }
+	void SleepState::breakdown()
+	{
+	}
 
-    void SleepState::tick(float dTime)
-    {
-        string output = "";
+	void SleepState::tick(float deltaTime)
+	{
+		string output = "";
 
-        _actor->increaseHealth(2);
-        _actor->increaseStamina(2);
+		_actor->increaseHealth(2);
+		_actor->increaseStamina(2);
 
-        if (_actor->isFullyRested())
-        {
-        	switchState("idle");
-        }
+		if (_actor->isFullyRested())
+		{
+			switchState("idle");
+		}
 
-        if(timeSlept < hoursToSleep)
-        {
-            if(timeSlept == 0)
-            {
-                output = _actor->getName() + " lays on the ground and begins to sleep.\n";
-            }
-            output + "zzzZZZ\n";
-            timeSlept++;
-        }
-    }
+		if (_timeSlept < _hoursToSleep)
+		{
+			if (_timeSlept == 0)
+			{
+				output = _actor->getName()
+						+ " lays on the ground and begins to sleep.\n";
+			}
+			output + "zzzZZZ\n";
+			_timeSlept++;
+		}
+	}
 
-    void SleepState::switchState(string nextState)
-    {
-    	_stateMachine->switchState(this, nextState);
-    }
+	void SleepState::switchState(string nextState)
+	{
+		_stateMachine->switchState(this, nextState);
+	}
 
 	string SleepState::toString()
 	{
 		return "sleep";
 	}
 }
+
 #endif
