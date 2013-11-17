b3c6e0d1fc2c5639616f356f23103f46b43adbae
bradley_crusco <bcrusco627@gmail.com>
2013-11-17 18:28:05 -0500

    More consistency and comment fixes. Docstyle comments added. Author comment added. White space fixed.

--- Source/States/BrawlState.h
+++ Source/States/BrawlState.h
@@ -1,3 +1,17 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *	Bradley Crusco
+ * Description:
+ * BrawlState header file.
+ *
+ */
+
 #ifndef BRAWLSTATE_H_
 #define DEFINE BRAWLSTATE_H_
 
@@ -10,42 +24,65 @@ typedef unsigned int UINT;
 
 namespace bammm
 {
-    class BrawlState : public State
-    {
-        public:
-            BrawlState(Actor* actor);
-            BrawlState(Actor* actor, IStateCallback* stateMachine);
-            void setup();
-            void breakdown();
-            void tick(float deltaTime);
+	class BrawlState: public State
+	{
+		public:
+			BrawlState(Actor* actor);
+			BrawlState(Actor* actor, IStateCallback* stateMachine);
+
+			/**
+			 setup
+			 @Pre-Condition- No input
+			 @Post-Condition- Sets up the state
+			 */
+			void setup();
+
+			/**
+			 breakdown
+			 @Pre-Condition- No input
+			 @Post-Condition- Performs a breakdown on the state
+			 */
+			void breakdown();
+
+			/**
+			 tick
+			 @Pre-Condition- Takes in a float deltaTime
+			 @Post-Condition- Executes a tick of length deltaTime
+			 */
+			void tick(float deltaTime);
+
+			/**
+			 toString
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns a string representation of the state
+			 */
 			string toString();
+	};
 
-        private:
-    };
-
-    BrawlState::BrawlState(Actor* actor)
-    {
+	BrawlState::BrawlState(Actor* actor)
+	{
 		_actor = actor;
-    }
+	}
 
-    BrawlState::BrawlState(Actor* actor, IStateCallback* stateMachine)
+	BrawlState::BrawlState(Actor* actor, IStateCallback* stateMachine)
 	{
 		_actor = actor;
 		registerTransitionCallback(stateMachine);
 	}
 
-    void BrawlState::setup()
-    {
-    }
+	void BrawlState::setup()
+	{
+	}
 
-    void BrawlState::breakdown()
-    {
-    }
+	void BrawlState::breakdown()
+	{
+	}
 
-    void BrawlState::tick(float deltaTime)
-    {
-        cout << _actor->getName() << " pulls his right arm back and then thrusts his arm forward by twisting his core. \"DWARF PUNCHHHH!\"\n";
-    }
+	void BrawlState::tick(float deltaTime)
+	{
+		cout << _actor->getName()
+				<< " pulls his right arm back and then thrusts his arm forward by twisting his core. \"DWARF PUNCHHHH!\"\n";
+	}
 
 	string BrawlState::toString()
 	{
