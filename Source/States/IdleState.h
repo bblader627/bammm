45c9d6f6ff127a4c902dd32ac07eb355287e9f47
bradley_crusco <bcrusco627@gmail.com>
2013-11-17 18:36:09 -0500

    Added author comments, added docstyle comments, fixed whitespace issues, changed variable names to match our style guide.

--- Source/States/IdleState.h
+++ Source/States/IdleState.h
@@ -1,3 +1,16 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *	Bradley Crusco
+ * Description:
+ * IdleState header file.
+ *
+ */
 #ifndef IDLESTATE_H_
 #define IDLESTATE_H_
 
@@ -10,46 +23,69 @@ typedef unsigned int UINT;
 
 namespace bammm
 {
-    class IdleState : public State
-    {
-        public:
-            IdleState(Actor* actor);
-            IdleState(Actor* actor, IStateCallback* statemachine);
-            void setup();
-            void breakdown();
-            void tick(float dTime);
-			string toString();
+	class IdleState: public State
+	{
+		public:
+			IdleState(Actor* actor);
+			IdleState(Actor* actor, IStateCallback* stateMachine);
 
-        private:
-    };
+			/**
+			 setup
+			 @Pre-Condition- No input
+			 @Post-Condition- Sets up the state
+			 */
+			void setup();
+
+			/**
+			 breakdown
+			 @Pre-Condition- No input
+			 @Post-Condition- Performs a breakdown on the state
+			 */
+			void breakdown();
+
+			/**
+			 tick
+			 @Pre-Condition- Takes in a float deltaTime
+			 @Post-Condition- Executes a tick of length deltaTime
+			 */
+			void tick(float deltaTime);
+
+			/**
+			 toString
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns a string representation of the state
+			 */
+			string toString();
+	};
 
 	IdleState::IdleState(Actor* actor)
 	{
 		_actor = actor;
 	}
 
-	IdleState::IdleState(Actor* actor, IStateCallback* statemachine)
+	IdleState::IdleState(Actor* actor, IStateCallback* stateMachine)
 	{
 		_actor = actor;
-		registerTransitionCallback(statemachine);
+		registerTransitionCallback(stateMachine);
 	}
 
-    void IdleState::setup()
-    {
-    }
+	void IdleState::setup()
+	{
+	}
 
-    void IdleState::breakdown()
-    {
-    }
+	void IdleState::breakdown()
+	{
+	}
 
-    void IdleState::tick(float dTime)
-    {
-    }
+	void IdleState::tick(float deltaTime)
+	{
+	}
 
 	string IdleState::toString()
 	{
 		return "idle";
 	}
 }
+
 #endif
 
