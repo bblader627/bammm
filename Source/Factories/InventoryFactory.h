f191f13a1901140e6e436b457b3940b4bf2c5ed8
ahome92 <xzero72gmail.com>
2013-11-17 18:41:33 -0500

    created Inventory Factory

---
+++ Source/Factories/InventoryFactory.h
@@ -0,0 +1,30 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *
+ * Description:
+ * InventoryFactory header file.
+ *
+ * Last Modified: Alvaro Home
+ *
+ */
+#ifndef INVENTORYFACTORY_H_
+#define INVENTORYFACTORY_H_
+
+#include <iostream>
+
+using namespace std;
+
+namespace bammm
+{
+	class InventoryFactory
+	{
+
+	};
+}
+#endif
+
