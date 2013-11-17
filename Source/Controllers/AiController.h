d4a8d016ae2613de83670c4d0963b373f4cde325
bradley_crusco <bcrusco627@gmail.com>
2013-11-17 13:45:07 -0500

    Fixes for naming conventions, comments added, author comment fixed, white space fixed, organization fixed.

--- Source/Controllers/AiController.h
+++ Source/Controllers/AiController.h
@@ -6,112 +6,123 @@
  * 	Matt Konstantinou
  * 	Michael Abramo
  *	Matt Witkowski	
- *   Bradley Crusco
+ *  Bradley Crusco
  * Description:
  * AiController header file.
  *
- * Last Modified: Matt Witkowski
- *
  */
-
 
 #ifndef AICONTROLLER_H_
 #define AICONTROLLER_H_
 
+#include <random>
 #include "Controller.h"
 #include "../States/StateMachine.h"
 #include "../SceneManager/SceneManager.h"
 #include "../Weapons/MeleeCombat.h"
 #include "../Weapons/RangedCombat.h"
-#include <random>
 
 #ifndef NULL
 #define NULL ((void *)0)
 #endif
 
 using namespace std;
-using namespace bammm;
 
 namespace bammm
 {
-    class AiController : public Controller
-    {
+	class AiController: public Controller
+	{
 		private:
-			MeleeCombat* meleeCombat;
-			RangedCombat* rangedCombat;
-			SceneManager* sceneManager;
-        public:
-            AiController(SceneManager* scene, MeleeCombat* meleeC);
-            void setup(Actor* actor);
-			bool update(float dTime);
-            ~AiController();
-    };
+			MeleeCombat* _meleeCombat;
+			RangedCombat* _rangedCombat;
+			SceneManager* _sceneManager;
 
-    AiController::AiController(SceneManager* scene, MeleeCombat* meleeC)
-    {
-		sceneManager = scene;
-		meleeCombat = meleeC;
-    }
+		public:
+			AiController(SceneManager* scene, MeleeCombat* meleeCombat);
+			~AiController();
 
-    void AiController::setup(Actor* actor)
-    {
-        _actor = actor;
-        _states = new HashMap<State*>();
-        _stateMachine = new StateMachine(_actor, _states);
+			/**
+			 setup
+			 @Pre-Condition- Takes an actor as input
+			 @Post-Condition- Actor passed as argument is setup for use
+			 */
+			void setup(Actor* actor);
+
+			/**
+			 update
+			 @Pre-Condition- Takes a float delta time (change in time) as input
+			 @Post-Condition- Updates are executed based on the deltaTime
+			 */
+			bool update(float deltaTime);
+	};
+
+	AiController::AiController(SceneManager* scene, MeleeCombat* meleeCombat)
+	{
+		_sceneManager = scene;
+		_meleeCombat = meleeCombat;
+	}
+
+	AiController::~AiController()
+	{
+		DynamicArray<State*>* temp = _states->getAllValues();
+		for (int i = 0; i < (int) temp->getSize(); i++)
+		{
+			delete temp->get(i);
+		}
+
+		delete _states;
+		delete _stateMachine;
+	}
+
+	void AiController::setup(Actor* actor)
+	{
+		_actor = actor;
+		_states = new HashMap<State*>();
+		_stateMachine = new StateMachine(_actor, _states);
 
 		//Create the states
-        //DO NOT DELETE THE REFERENCES TO STATEMACHINE.  THE CODE WILL SEG FAULT IF YOU DO
-        DrinkState* drinkState = new DrinkState(_actor, _stateMachine);
-        MineState* mineState = new MineState(_actor, _stateMachine);
-        SingState* singState = new SingState(_actor, _stateMachine);
-        BrawlState* brawlState = new BrawlState(_actor, _stateMachine);
-        SleepState* sleepState = new SleepState(_actor, _stateMachine);
-        IdleState* idleState = new IdleState(_actor, _stateMachine);
+		//DO NOT DELETE THE REFERENCES TO STATEMACHINE. THE CODE WILL SEG FAULT IF YOU DO
+		DrinkState* drinkState = new DrinkState(_actor, _stateMachine);
+		MineState* mineState = new MineState(_actor, _stateMachine);
+		SingState* singState = new SingState(_actor, _stateMachine);
+		BrawlState* brawlState = new BrawlState(_actor, _stateMachine);
+		SleepState* sleepState = new SleepState(_actor, _stateMachine);
+		IdleState* idleState = new IdleState(_actor, _stateMachine);
 		CombatState* combatState = new CombatState(_actor, _stateMachine);
 
 		//Put actor in idle state
 		_stateMachine->initialState(idleState);
 
-        _states->add(idleState->to_string(), idleState);
-        _states->add(mineState->to_string(), mineState);
-        _states->add(drinkState->to_string(), drinkState);
-        _states->add(singState->to_string(), singState);
-        _states->add(brawlState->to_string(), brawlState);
-        _states->add(sleepState->to_string(), sleepState);
-       	_states->add(combatState->to_string(), combatState);
-    }
+		_states->add(idleState->to_string(), idleState);
+		_states->add(mineState->to_string(), mineState);
+		_states->add(drinkState->to_string(), drinkState);
+		_states->add(singState->to_string(), singState);
+		_states->add(brawlState->to_string(), brawlState);
+		_states->add(sleepState->to_string(), sleepState);
+		_states->add(combatState->to_string(), combatState);
+	}
 
-    AiController::~AiController()
-    {
-        DynamicArray<State*>* temp = _states->getAllValues();
-        for(int i = 0; i < (int)temp->getSize(); i++)
-        {
-            delete temp->get(i);
-        }
-
-        delete _states;
-        delete _stateMachine;
-    }
-
-	bool AiController::update(float dTime)
+	bool AiController::update(float deltaTime)
 	{
-    	//all currently running states
-		if(_actor->getHealth() <= 0)
+		//All currently running states
+		if (_actor->getHealth() <= 0)
 		{
-			sceneManager->getSceneGraph()->remove(_actor->getLocation(), _actor);
+			_sceneManager->getSceneGraph()->remove(_actor->getLocation(),
+					_actor);
 			delete _actor;
 			return true;
 		}
 		else
 		{
-			Vector3D* newLoc = new Vector3D(0,0,0);
+			Vector3D* newLoc = new Vector3D(0, 0, 0);
 			cout << "Before Graph\n";
-			sceneManager->getSceneGraph()->moveTowards(_actor, newLoc);
+			_sceneManager->getSceneGraph()->moveTowards(_actor, newLoc);
 			cout << "After Graph\n";
-        	_stateMachine->tick(dTime);
+			_stateMachine->tick(deltaTime);
 		}
 
 		return false;
-    }
+	}
 }
+
 #endif
