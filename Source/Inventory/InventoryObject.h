f322c163948bb1b5d54f826aafe8b8129b1f0a70
ahome92 <xzero72gmail.com>
2013-11-17 15:26:00 -0500

    Added InventoryObject header

---
+++ Source/Inventory/InventoryObject.h
@@ -0,0 +1,29 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *
+ * Description:
+ * InventoryObject header file.
+ *
+ * Last Modified: Alvaro Home
+ *
+ */
+#ifndef INVENTORYOBJECT_H_
+#define INVENTORYOBJECT_H_
+
+#include <iostream>
+
+using namespace std;
+
+namespace bammm
+{
+	class InventoryObject
+	{
+	};
+}
+#endif
+
