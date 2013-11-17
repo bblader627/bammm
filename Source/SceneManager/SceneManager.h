381ea3909f09368fa6059e41852d15fc2796b2f9
bradley_crusco <bcrusco627@gmail.com>
2013-11-17 17:07:51 -0500

    Updated to fix consistency/comment issues. Reorganized, a couple of names changed, white space issues fixed, and new docsytle comments added.

--- Source/SceneManager/SceneManager.h
+++ Source/SceneManager/SceneManager.h
@@ -4,6 +4,7 @@
 #include "Grid3D.h"
 #include "../Actors/Actor.h"
 #include "ITickable.h"
+
 using namespace std;
 
 namespace bammm
@@ -13,29 +14,65 @@ namespace bammm
 		private:
 			DynamicArray<Actor*>* _allActors;
 			DynamicArray<ITickable*>* _allTickables;
-			Grid3d<Actor*>* _sceneGraph;
+			Grid3D<Actor*>* _sceneGraph;
 			static const int SCENE_X = 10;
 			static const int SCENE_Y = 10;
 			static const int SCENE_Z = 10;
 
 		public:
-			~SceneManager();
 			SceneManager();
-			void addActor(Actor* actor);
-			void removeActor(Actor* actor);
-			void addTickable(ITickable* tickable);
-			void removeTickable(ITickable* tickable);
-			string to_string();
-			Grid3d<Actor*>* getSceneGraph();
-	};
+			~SceneManager();
 
-	SceneManager::~SceneManager()
-	{
-	}
+			/**
+			 addActor
+			 @Pre-Condition- Takes in an actor
+			 @Post-Condition- Adds the actor to the scene
+			 */
+			void addActor(Actor* actor);
+
+			/**
+			 removeActor
+			 @Pre-Condition- Takes in an actor
+			 @Post-Condition- Removes the given actor from the scene
+			 */
+			void removeActor(Actor* actor);
+
+			/**
+			 addTickable
+			 @Pre-Condition- Takes in a tickable
+			 @Post-Condition- Adds the given tickable to the scene
+			 */
+			void addTickable(ITickable* tickable);
+
+			/**
+			 removeTickable
+			 @Pre-Condition- Takes in a tickable
+			 @Post-Condition- Removes the given tickable from the scene
+			 */
+			void removeTickable(ITickable* tickable);
+
+			/**
+			 getSceneGraph
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns the Grid3D of the scene
+			 */
+			Grid3D<Actor*>* getSceneGraph();
+
+			/**
+			 toString
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns an empty string right now
+			 */
+			string toString();
+	};
 
 	SceneManager::SceneManager()
 	{
-		_sceneGraph = new Grid3d<Actor*>(SCENE_X, SCENE_Y, SCENE_Z);
+		_sceneGraph = new Grid3D<Actor*>(SCENE_X, SCENE_Y, SCENE_Z);
+	}
+
+	SceneManager::~SceneManager()
+	{
 	}
 
 	void SceneManager::addActor(Actor* actor)
@@ -45,7 +82,7 @@ namespace bammm
 
 	void SceneManager::removeActor(Actor* actor)
 	{
-		_allActors->removeElem(actor);
+		_allActors->removeElement(actor);
 	}
 
 	void SceneManager::addTickable(ITickable* tickable)
@@ -55,17 +92,18 @@ namespace bammm
 
 	void SceneManager::removeTickable(ITickable* tickable)
 	{
-		_allTickables->removeElem(tickable);
+		_allTickables->removeElement(tickable);
 	}
 
-	Grid3d<Actor*>* SceneManager::getSceneGraph()
+	Grid3D<Actor*>* SceneManager::getSceneGraph()
 	{
 		return _sceneGraph;
 	}
 
-	string SceneManager::to_string()
+	string SceneManager::toString()
 	{
 		return "";
 	}
 }
+
 #endif
