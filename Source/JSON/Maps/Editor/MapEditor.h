93a0a4a0b25660f043c69285d1df0012140e9f30
Matt Witkowski <mwitkows@stevens.edu>
2013-11-18 02:08:21 -0500

    Created beta BAMMM Map editor

--- Source/JSON/Maps/Editor/MapEditor.h
+++ Source/JSON/Maps/Editor/MapEditor.h
@@ -2,22 +2,46 @@
 #define MAPEDITOR_H_
 
 #include "../../JSONParser.h"
-#include <iostream> 
+#include <iostream>
+#include <string>
 namespace bammm
 {
 	class MapEditor
 	{
 		private:
-			JSONParser parser;
+			int _x;
+			int _y;
+			int _z;
+			int _cursorX;
+			int _cursorY;
+			int _cursorZ;
+			string _name;
+			char _grid[10][10][10];
+			JSONParser _parser;
 		public:
 			MapEditor();
 			~MapEditor();
 			bool loadMap(string filename);
-			bool saveMap(string filename);
+			bool saveMap();
 			void createMap(int x, int y, int z);
 			void startEditor();
 			void removeItem(int x, int y, int z);
-			void addItem(string item);
+			void addItem(int x, int y, int z, char item);
+			void displayMap();
+			void displayHelp();
+			void moveUp();
+			void moveDown();
+			void moveLeft();
+			void moveRight();
+			void moveZUp();
+			void moveZDown();
+			void displayCoordinates();
+			string createJsonObject(int x, int y, int z, string name, string walkable);
+			string createJson();
+			string createWaterJson();
+			string createBarrierJson();
+			string createMineJson();
+			string createBuildingJson();
 	};
 }
 
