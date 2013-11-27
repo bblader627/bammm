ee9c1425cb81e7b245af975eeefa65451e47cdc2
Bradley Crusco <bcrusco627@gmail.com>
2013-11-27 17:14:42 -0500

    Split off source code from MovementState.h to MovementState.cpp

--- Source/States/MovementState.h
+++ Source/States/MovementState.h
@@ -74,45 +74,6 @@ namespace bammm
 			 */
 			string toString();
 	};
-
-	MovementState::MovementState(Actor& actor)
-	{
-		_actor = &actor;
-	}
-
-	MovementState::MovementState(Actor& actor, IStateCallback& stateMachine, Grid3D<Actor*>& sceneGraph )
-	{
-		_sceneGraph = &sceneGraph;
-		_actor = &actor;
-		registerTransitionCallback(stateMachine);
-	}
-
-	void MovementState::setup()
-	{
-		//_direction = &direction;
-	}
-
-	void MovementState::breakdown()
-	{
-	}
-
-	void MovementState::tick(float deltaTime)
-	{
-		Vector3D* newLoc = new Vector3D(0,0,0);
-		_sceneGraph->moveTowards(_actor, newLoc);
-		//SceneManager::getSceneGraph().moveTowards(_actor, newLoc);
-		//switchState("idle");
-	}
-
-	void MovementState::switchState(string nextState)
-	{
-		_stateMachine->switchState(this, nextState);
-	}
-
-	string MovementState::toString()
-	{
-		return "movement";
-	}
 }
 
 #endif
