b6b8316b2cb14d712080cadd7522c87cf75239c3
Bradley Crusco <bcrusco627@gmail.com>
2013-11-27 17:18:13 -0500

    Split out source from SleepState.h to SleepState.cpp

---
+++ Source/States/SleepState.cpp
@@ -0,0 +1,72 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *	Bradley Crusco
+ * Description:
+ * SleepState cpp file.
+ *
+ */
+
+#include "SleepState.h"
+
+namespace bammm
+{
+	SleepState::SleepState(Actor& actor)
+	{
+		_actor = &actor;
+	}
+
+	SleepState::SleepState(Actor& actor, IStateCallback& stateMachine)
+	{
+		_actor = &actor;
+		registerTransitionCallback(stateMachine);
+	}
+
+	void SleepState::setup()
+	{
+		_timeSlept = 0;
+	}
+
+	void SleepState::breakdown()
+	{
+	}
+
+	void SleepState::tick(float deltaTime)
+	{
+		string output = "";
+
+		_actor->increaseHealth(2);
+		_actor->increaseStamina(2);
+
+		if (_actor->isFullyRested())
+		{
+			switchState("idle");
+		}
+
+		if (_timeSlept < _hoursToSleep)
+		{
+			if (_timeSlept == 0)
+			{
+				output = _actor->getName()
+						+ " lays on the ground and begins to sleep.\n";
+			}
+			output + "zzzZZZ\n";
+			_timeSlept++;
+		}
+	}
+
+	void SleepState::switchState(string nextState)
+	{
+		_stateMachine->switchState(this, nextState);
+	}
+
+	string SleepState::toString()
+	{
+		return "sleep";
+	}
+}
--- Source/States/SleepState.h
+++ Source/States/SleepState.h
@@ -72,60 +72,6 @@ namespace bammm
 			 */
 			string toString();
 	};
-
-	SleepState::SleepState(Actor& actor)
-	{
-		_actor = &actor;
-	}
-
-	SleepState::SleepState(Actor& actor, IStateCallback& stateMachine)
-	{
-		_actor = &actor;
-		registerTransitionCallback(stateMachine);
-	}
-
-	void SleepState::setup()
-	{
-		_timeSlept = 0;
-	}
-
-	void SleepState::breakdown()
-	{
-	}
-
-	void SleepState::tick(float deltaTime)
-	{
-		string output = "";
-
-		_actor->increaseHealth(2);
-		_actor->increaseStamina(2);
-
-		if (_actor->isFullyRested())
-		{
-			switchState("idle");
-		}
-
-		if (_timeSlept < _hoursToSleep)
-		{
-			if (_timeSlept == 0)
-			{
-				output = _actor->getName()
-						+ " lays on the ground and begins to sleep.\n";
-			}
-			output + "zzzZZZ\n";
-			_timeSlept++;
-		}
-	}
-
-	void SleepState::switchState(string nextState)
-	{
-		_stateMachine->switchState(this, nextState);
-	}
-
-	string SleepState::toString()
-	{
-		return "sleep";
-	}
 }
 
 #endif
