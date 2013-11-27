cc318b69290546d277cf52aa2e03e62d367251aa
Bradley Crusco <bcrusco627@gmail.com>
2013-11-27 16:54:35 -0500

    Split source out of SceneManager.h into ScenemManager.cpp. Reorganized code, updated docstyle comments, fixed whitespace, etc.

--- Source/SceneManager/SceneManager.h
+++ Source/SceneManager/SceneManager.h
@@ -1,12 +1,27 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *  Bradley Crusco
+ * Description:
+ * SceneManager header file.
+ *
+ */
+
 #ifndef SCENEMANAGER_H_
 #define SCENEMANAGER_H_
 
+#include <random>
 #include "Grid3D.h"
 #include "../Actors/Actor.h"
 #include "../Controllers/AiController.h"
 #include "../Interfaces/ITickable.h"
 #include "../Weapons/MeleeCombat.h"
-#include <random>
+
 using namespace std;
 
 namespace bammm
@@ -23,107 +38,65 @@ namespace bammm
 			Grid3D<Actor*> _sceneGraph;
 
 		public:
-			virtual ~SceneManager();
 			SceneManager();
+			virtual ~SceneManager();
 
+			/**
+			 addActor
+			 @Pre-Condition- Takes in an actor
+			 @Post-Condition- Adds the actor to the scene
+			 */
 			void addActor(Actor* actor);
+
+			/**
+			 removeActor
+			 @Pre-Condition- Takes in an actor
+			 @Post-Condition- Removes the given actor from the scene
+			 */
 			void removeActor(Actor* actor);
+
+			/**
+			 addTickable
+			 @Pre-Condition- Takes in a tickable
+			 @Post-Condition- Adds the given tickable to the scene
+			 */
 			void addTickable(ITickable* tickable);
+
+			/**
+			 removeTickable
+			 @Pre-Condition- Takes in a tickable
+			 @Post-Condition- Removes the given tickable from the scene
+			 */
 			ITickable* removeTickable(ITickable* tickable);
+
+			/**
+			 setMeleeCombat
+			 @Pre-Condition- Takes in an instance of MeleeCombat
+			 @Post-Condition- Sets meleeCombat to the scene
+			 */
 			void setMeleeCombat(MeleeCombat& meleeCombat);
+
+			/**
+			 toString
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns an empty string right now
+			 */
 			string toString();
+
+			/**
+			 getSceneGraph
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns the Grid3D of the scene
+			 */
 			Grid3D<Actor*>& getSceneGraph();
+
+			/**
+			 tick
+			 @Pre-Condition- Takes in a float deltaTime
+			 @Post-Condition- Ticks the scene for the duration of deltaTime
+			 */
 			virtual void tick(float deltaTime);
-
 	};
-
-	//Grid3D<Actor*> SceneManager::_sceneGraph(SCENE_X, SCENE_Y, SCENE_Z);
-
-	SceneManager::~SceneManager()
-	{
-	}
-
-	SceneManager::SceneManager(): _sceneGraph(SCENE_X, SCENE_Y, SCENE_Z)
-	{
-	    Vector3D* temp;
-	    //Random number generator
-	    random_device rd;
-	    mt19937 generator(rd());
-
-	    //Pick random number of orcs
-	    int minOrc = 1;
-	    int maxOrc = 10;
-	    uniform_int_distribution<int> orcDistribution (minOrc, maxOrc);
-	    int orcCount = orcDistribution(generator);
-
-	    //Create the orcs
-	    uniform_int_distribution<int> xDistribution (0, getSceneGraph().getX() - 1);
-	    uniform_int_distribution<int> yDistribution (0, getSceneGraph().getY() - 1);
-	    for(int i = 0; i < orcCount; i++)
-	    {
-	        int randomX = xDistribution(generator);
-	        int randomY = yDistribution(generator);
-	        temp = new Vector3D(randomX, randomY, 0);
-
-	        Actor* newOrc = new Actor("Dick", "orc");
-	        addActor(newOrc);
-
-	        AiController* newAi = new AiController();
-	        newAi->setup(*_allActors.get(i), *_meleeCombat, _sceneGraph);
-	        getSceneGraph().add(temp, (_allActors.get(i)));
-	        addTickable(newAi);
-	    }
-	}
-
-	void SceneManager::setMeleeCombat(MeleeCombat& meleeCombat)
-	{
-	    _meleeCombat = &meleeCombat;
-	}
-
-	void SceneManager::addActor(Actor* actor)
-	{
-		_allActors.add(actor);
-	}
-
-	void SceneManager::removeActor(Actor* actor)
-	{
-		_allActors.removeElement(actor);
-	}
-
-	void SceneManager::addTickable(ITickable* tickable)
-	{
-		_allTickables.add(tickable);
-	}
-
-	ITickable* SceneManager::removeTickable(ITickable* tickable)
-	{
-		_allTickables.removeElement(tickable);
-		return tickable;
-	}
-
-	Grid3D<Actor*>& SceneManager::getSceneGraph()
-	{
-		return _sceneGraph;
-	}
-
-	void SceneManager::tick(float deltaTime)
-	{
-		cout << "I was ticked\n";
-		int size = _allTickables.getSize();
-		for(int i = 0; i < size; i++)
-		{
-			_allTickables.get(i)->tick(deltaTime);
-			if(_allTickables.get(i)->canDelete())
-			{
-			    delete removeTickable(_allTickables.get(i));
-			}
-		}
-	}
-
-	string SceneManager::toString()
-	{
-		return "";
-	}
 }
 
 #endif
