f4cddb57d24684340e9ad3f3903e89540998efa8
Bradley Crusco <bcrusco627@gmail.com>
2013-11-27 18:00:15 -0500

    Added proper comments, fixed and added new docstyle comments, fixed variable naming schemes, fixed white space issues, added one todo.

--- Source/JSON/JSONParser.h
+++ Source/JSON/JSONParser.h
@@ -27,7 +27,6 @@
 #define NULL (void *) 0
 #endif
 
-using namespace bammm;
 using namespace std;
 
 namespace bammm
@@ -43,7 +42,6 @@ namespace bammm
 			 @Pre-Condition- Takes in a JSON node
 			 @Post-Condition- Adds the node as a root node
 			 */
-
 			void addRoot(JSON & newNode);
 
 			/*
@@ -51,20 +49,17 @@ namespace bammm
 			 @Pre-Condition- Takes in a JSON  rootNode and a JSON childNode
 			 @Post-Condition- Adds the child to the specified rootNode
 			 */
-
 			void addChild(JSON & rootNode, JSON & newNode);
 
 		public:
-
 			JSONParser();
 
 			/*
 			 parseFile
-			 @Pre-Condition- Takes in a fileName
+			 @Pre-Condition- Takes in a filename
 			 @Post-Condition- Parses the file and returns true if successful
 			 */
-
-			bool parseFile(string fileName);
+			bool parseFile(string filename);
 
 			/*
 			 getMap
@@ -74,14 +69,18 @@ namespace bammm
 			HashMap<JSON*>* getMap();
 
 			/*
-
 			 getRootNode
 			 @Pre-Condition- Takes in the key of the desired JSON node
 			 @Post-Condition- Returns the JSON node with name "key"
 			 */
-
 			JSON* getRootNode(string key);
 
+			//TODO: Can this implementation be moved to the cpp file?
+			/*
+			 operator[]
+			 @Pre-Condition- Takes in const string key
+			 @Post-Condition- Returns JSON *
+			 */
 			JSON * operator[](const string key)
 			{
 				return _rootMap.getValue(key);
