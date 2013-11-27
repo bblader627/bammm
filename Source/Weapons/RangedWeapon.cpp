701bc55a876d9ebddd530a103be7b1bfc37ed528
Bradley Crusco <bcrusco627@gmail.com>
2013-11-27 17:31:02 -0500

    Created RangedWeapon.cpp for when RangedWeapon.h functions are implemented.

---
+++ Source/Weapons/RangedWeapon.cpp
@@ -0,0 +1,20 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *  Bradley Crusco
+ * Description:
+ * RangedWeapon cpp file.
+ *
+ */
+
+#include "RangedWeapon.h"
+
+namespace bammm
+{
+
+}
