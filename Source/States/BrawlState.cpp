d89d5e0f916f23d7180b5def6258efcea5e2f2c1
Bradley Crusco <bcrusco627@gmail.com>
2013-11-27 16:59:59 -0500

    Seperated source out of BrawlState.h into BrawlState.cpp

---
+++ Source/States/BrawlState.cpp
@@ -0,0 +1,48 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *	Bradley Crusco
+ * Description:
+ * BrawlState cpp file.
+ *
+ */
+
+#include "BrawlState.h"
+
+namespace bammm
+{
+	BrawlState::BrawlState(Actor& actor)
+	{
+		_actor = &actor;
+	}
+
+	BrawlState::BrawlState(Actor& actor, IStateCallback& stateMachine)
+	{
+		_actor = &actor;
+		registerTransitionCallback(stateMachine);
+	}
+
+	void BrawlState::setup()
+	{
+	}
+
+	void BrawlState::breakdown()
+	{
+	}
+
+	void BrawlState::tick(float deltaTime)
+	{
+		cout << _actor->getName()
+				<< " pulls his right arm back and then thrusts his arm forward by twisting his core. \"DWARF PUNCHHHH!\"\n";
+	}
+
+	string BrawlState::toString()
+	{
+		return "brawl";
+	}
+}
