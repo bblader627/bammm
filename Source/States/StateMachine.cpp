406ced26a50c5c45ff6d81e7eca423125ddad858
Bradley Crusco <bcrusco627@gmail.com>
2013-11-27 17:22:28 -0500

    Split out source from StateMachine.h to StateMachine.cpp, updated StateMachine.h author comment.

---
+++ Source/States/StateMachine.cpp
@@ -0,0 +1,90 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *	Bradley Crusco
+ * Description:
+ * StateMachine cpp file.
+ *
+ */
+
+#include "StateMachine.h"
+
+namespace bammm
+{
+	StateMachine::StateMachine()
+	{
+	}
+
+	StateMachine::~StateMachine()
+	{
+	}
+
+	StateMachine::StateMachine(Actor& actor, HashMap<State*>& allStates)
+	{
+		_actor = &actor;
+		_allStates = &allStates;
+	}
+
+	void StateMachine::setup(Actor& actor, HashMap<State*>& allStates)
+	{
+		_actor = &actor;
+		_allStates = &allStates;
+	}
+
+	void StateMachine::initialState(State* initial)
+	{
+		initial->setup();
+		currentStates.add(initial);
+	}
+
+	void StateMachine::tick(float dTime)
+	{
+		for (int i = 0; i < (int) currentStates.getSize(); i++)
+		{
+			currentStates.get(i)->tick(dTime);
+		}
+	}
+
+	void StateMachine::switchState(State& current, State& newState)
+	{
+		removeState(&current);
+		addState(&newState);
+	}
+
+	void StateMachine::switchState(State& current, string newStateString)
+	{
+		if (newStateString == "null")
+		{
+			removeState(&current);
+			return;
+		}
+		switchState(current, *_allStates->getValue(newStateString));
+	}
+
+	void StateMachine::addState(State* newState)
+	{
+		newState->setup();
+		currentStates.add(newState);
+	}
+
+	void StateMachine::removeState(State* oldState)
+	{
+		oldState->breakdown();
+		currentStates.removeElement(oldState);
+	}
+
+	DynamicArray<State*>& StateMachine::getCurrentStates()
+	{
+		return currentStates;
+	}
+
+	string StateMachine::toString()
+	{
+		return "statemachine";
+	}
+}
--- Source/States/StateMachine.h
+++ Source/States/StateMachine.h
@@ -5,13 +5,10 @@
  * 	Alvaro Home
  * 	Matt Konstantinou
  * 	Michael Abramo
- * 	Bradley Crusco
- * 	Matt Witkowski
- *
+ *	Matt Witkowski
+ *	Bradley Crusco
  * Description:
  * StateMachine header file.
- *
- * Last Modified: Matthew Konstantinou
  *
  */
 
@@ -96,78 +93,5 @@ namespace bammm
 			string toString();
 
 	};
-	
-	StateMachine::StateMachine()
-	{
-	}
-
-	StateMachine::~StateMachine()
-	{
-	}
-
-	StateMachine::StateMachine(Actor& actor, HashMap<State*>& allStates)
-	{
-		_actor = &actor;
-		_allStates = &allStates;
-	}
-
-	void StateMachine::setup(Actor& actor, HashMap<State*>& allStates)
-	{
-		_actor = &actor;
-		_allStates = &allStates;
-	}
-
-	void StateMachine::initialState(State* initial)
-	{
-		initial->setup();
-		currentStates.add(initial);
-	}
-
-	void StateMachine::tick(float dTime)
-	{
-		for(int i = 0; i < (int) currentStates.getSize(); i++)
-		{
-			currentStates.get(i)->tick(dTime);
-		}
-	}
-
-	void StateMachine::switchState(State& current, State& newState)
-	{
-		removeState(&current);
-		addState(&newState);
-	}
-
-	void StateMachine::switchState(State& current, string newStateString)
-	{
-		if (newStateString=="null")
-		{
-			removeState(&current);
-			return;
-		}
-		switchState(current, *_allStates->getValue(newStateString));
-	}
-
-	void StateMachine::addState(State* newState)
-	{
-		newState->setup();
-		currentStates.add(newState);
-	}
-
-	void StateMachine::removeState(State* oldState)
-	{
-		oldState->breakdown();
-		currentStates.removeElement(oldState);
-	}
-
-	DynamicArray<State*>& StateMachine::getCurrentStates()
-	{
-		return currentStates;
-	}
-
-
-	string StateMachine::toString()
-	{
-		return "statemachine";
-	}
 }
 #endif
