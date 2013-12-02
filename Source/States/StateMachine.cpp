2c24fe4a30841d3d7aaf49c6d58a7650c70a2172
Bradley Crusco <bcrusco627@gmail.com>
2013-12-01 20:43:51 -0500

    typo fix

--- Source/States/StateMachine.cpp
+++ Source/States/StateMachine.cpp
@@ -75,7 +75,7 @@ namespace bammm
 		{
 			if (newState->toString() == "combat")
 			{
-				//test for fight hapening
+				//test for fight happening
 				if (!_meleeCombat->fightHappening())
 				{
 					this->removeState(newState);
