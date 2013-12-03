49a54928edd6627017894864a772731e28aeeb10
bradley_crusco <bcrusco627@gmail.com>
2013-12-02 21:09:04 -0500

    Created LevelingSystem.cpp and .h

---
+++ Source/LevelingSystem/LevelingSystem.cpp
@@ -0,0 +1,20 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *  Bradley Crusco
+ * Description:
+ * LevelingSystem cpp file.
+ *
+ */
+
+#include "LevelingSystem.h"
+
+namespace bammm
+{
+
+}
---
+++ Source/LevelingSystem/LevelingSystem.h
@@ -0,0 +1,33 @@
+/*
+ * CS585
+ *
+ * Team Bammm
+ * 	Alvaro Home
+ * 	Matt Konstantinou
+ * 	Michael Abramo
+ *	Matt Witkowski
+ *  Bradley Crusco
+ * Description:
+ * LevelingSystem header file.
+ *
+ */
+
+#ifndef LEVELINGSYSTEM_H_
+#define LEVELINGSYSTEM_H_
+
+namespace bammmm
+{
+	class LevelingSystem
+	{
+			//static everything
+		private:
+			//return actor?
+			static void levelUP();
+		public:
+			//return actor?
+			static void gainExperience();
+
+	};
+}
+
+#endif
