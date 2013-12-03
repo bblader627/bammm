25d41ae90f3cfa731b5ba250615ed9d878510156
Bradley Crusco <bcrusco627@gmail.com>
2013-12-03 16:27:44 -0500

    Fixed typo

--- Source/Factories/Factory.h
+++ Source/Factories/Factory.h
@@ -65,7 +65,7 @@ namespace bammm
 			 @Pre-Condition- accepts pointer to root JSON node and type string and map
 			 @Post-Condition- Data is parsed and stored in map
 			 */
-			void parseRangedWeapontoWeaponData(JSON* rootNode, string type,
+			void parseRangedWeaponToWeaponData(JSON* rootNode, string type,
 					HashMap<WeaponData>* map);
 
 		public:
