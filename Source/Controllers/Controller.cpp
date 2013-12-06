9ebd003fe20c8c361cc67832637171189687ca97
bradley_crusco <bcrusco627@gmail.com>
2013-12-05 23:37:01 -0500

    Final updates to controller

--- Source/Controllers/Controller.cpp
+++ Source/Controllers/Controller.cpp
@@ -24,6 +24,7 @@ namespace bammm
 	{
 		DynamicArray<string>* keys = _states.getAllKeys();
 		int size = keys->getSize();
+
 		for (int i = 0; i < size; i++)
 		{
 			delete _states.getValue(keys->get(i));
