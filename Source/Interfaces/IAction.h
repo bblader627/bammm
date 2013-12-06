2ac061948f569891d3d8a936d90d6b20b3751a5a
bradley_crusco <bcrusco627@gmail.com>
2013-12-06 00:29:37 -0500

    Added all the missing docstyle comments, other consistency issues fixed too. (mostly white space)

--- Source/Interfaces/IAction.h
+++ Source/Interfaces/IAction.h
@@ -16,7 +16,9 @@
 #define IACTION_H_
 
 #include <iostream>
+
 class Actor;
+
 using namespace std;
 
 namespace bammm
@@ -26,20 +28,48 @@ namespace bammm
 		private:
 			string _type;
 			int _amount;
+
 		public:
+			/*
+			 action
+			 @Pre-Condition- Takes in an Actor* target, and an int amount
+			 @Post-Condition- Returns void
+			 */
 			virtual void action(Actor* target, int amount)=0;
 
 			/*
-				
-			*/
+			 setType
+			 @Pre-Condition- Takes in a string type
+			 @Post-Condition- Returns void
+			 */
 			void setType(string type);
-			
+
+			/*
+			 setAmount
+			 @Pre-Condition- Takes in an int amount
+			 @Post-Condition- Returns void
+			 */
 			void setAmount(int amount);
-			
+
+			/*
+			 increaseAmount
+			 @Pre-Condition- Takes in an int amount
+			 @Post-Condition- Returns void
+			 */
 			void increaseAmount(int amount);
 
+			/*
+			 decreaseAmount
+			 @Pre-Condition- Takes in an int amount
+			 @Post-Condition- Returns void
+			 */
 			void decreaseAmount(int amount);
 
+			/*
+			 toString
+			 @Pre-Condition- No input
+			 @Post-Condition- Returns a string representation of the IAction
+			 */
 			string toString();
 	};
 }
